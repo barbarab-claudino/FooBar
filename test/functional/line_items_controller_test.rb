b1926d0ce4b92c5b4be5ff7184c429b262c4b023
Barbara Barbosa C. da Silva <barbarab.claudino@gmail.com>
2012-12-16 16:58:45 +0100

    concerto do carrinho
    (ainda naume stá terminado)

--- test/functional/line_items_controller_test.rb
+++ test/functional/line_items_controller_test.rb
@@ -2,7 +2,7 @@ require 'test_helper'
 
 class LineItemsControllerTest < ActionController::TestCase
   setup do
-    @line_item = line_items(:one)
+    @line_item = line_items(:line_item)
   end
 
   test "should get index" do
@@ -18,7 +18,7 @@ class LineItemsControllerTest < ActionController::TestCase
 
   test "should create line_item" do
     assert_difference('LineItem.count') do
-      post :create, product_id: products(:ruby).id
+      post :create, product_id: products(:code).id
     end
 
     assert_redirected_to cart_path(assigns(:line_item).cart)
